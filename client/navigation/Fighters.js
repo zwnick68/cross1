63e400ab66433225111fd331f4efd62edc7f722b
Nicholas Zwiren <zwnick68@gmail.com>
2022-06-14 18:08:51 -0400

    added serializer for weightlcasses to access fighters

--- client/navigation/Fighters.js
+++ client/navigation/Fighters.js
@@ -1,6 +1,5 @@
 import React,{useEffect,useState} from 'react';
 import {View, Text} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
 import ModalSelector from 'react-native-modal-selector'
 
 
@@ -19,28 +18,31 @@ export default function Fighters ({navigation}) {
             console.log(error)
         }
        }, [])
-       console.log(weightclass[0].id)
-       let index = 0;
-       const data = [
+     console.log(weightclass)
+    //    let index = 0;
+        const data = [
            { key: null, section: true, label: 'Choose Weightclass'},
-           { key: weightclass[0].id, label: 'Flyweight' },
-           { key: weightclass[1].id, label: 'Bantamweight' },
-           { key: weightclass[2].id, label: 'Featherweight' },
-           { key: weightclass[3].id, label: 'Lightweight' },
-           { key: weightclass[4].id, label: 'Welterweight' },
-           { key: weightclass[5].id, label: 'Middleweight' },
-           { key: weightclass[6].id, label: 'Light Heavyweight' },
-           { key: weightclass[7].id, label: 'Heavyweight' },
-           { key: weightclass[8].id, label: "Womens's Strawweight" },
-           { key: weightclass[9].id, label: "Womens's Flyweight" },
-           { key: weightclass[10].id, label: "Womens's Bantamweight" },
-           { key: weightclass[11].id, label: "Womens's Featherweight" }
+           { key: weightclass[0], label: 'Flyweight' },
+           { key: weightclass[1], label: 'Bantamweight' },
+           { key: weightclass[2], label: 'Featherweight' },
+           { key: weightclass[3], label: 'Lightweight' },
+           { key: weightclass[4], label: 'Welterweight' },
+           { key: weightclass[5], label: 'Middleweight' },
+           { key: weightclass[6], label: 'Light Heavyweight' },
+           { key: weightclass[7], label: 'Heavyweight' },
+           { key: weightclass[8], label: "Womens's Strawweight" },
+           { key: weightclass[9], label: "Womens's Flyweight" },
+           { key: weightclass[10], label: "Womens's Bantamweight" },
+           { key: weightclass[11], label: "Womens's Featherweight" }
        ];
 
     return(
+     
         <ModalSelector
                     data={data}
                     initValue="Weightclasses"
-                    onChange={(option)=>{ alert(`${option.label} has been chosen`) }} />
+                    visible={false}
+                    onChange={(option)=>{ alert(`${option.key.fighters[0].name} has been chosen`) }}
+                    keyExtractor={(data) => data.key } />
     )
 }
